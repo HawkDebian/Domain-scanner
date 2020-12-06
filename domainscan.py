7db0e82573ca58f66ad61b80f335833a92718051
V1 MoDzZz <67358940+V1MoDzZz@users.noreply.github.com>
2020-12-06 14:01:42 +0500

    Create domainscan.py
    Requirements:
    
    Kali linux
    
    Python3

---
+++ domainscan.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+os.system('clear')
+
+sys.ps1 = '\033[01;32m '
+
+print(sys.ps1)
+
+print('''
+
+ ####### ######   #####   #####  
+    #    #     # #     # #     # 
+    #    #     #       #       # 
+    #    ######   #####   #####  
+    #    #   #         #       # 
+    #    #    #  #     # #     # 
+    #    #     #  #####   #####  
+                                 
+
+''')
+
+
+lol = input('Enter the domain ya wanna scan: ')
+
+os.system('whois '+lol)
+os.system('dig '+lol)
